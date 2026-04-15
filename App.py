673b71e71343cbac14b88e1a5f09ab1845d37b85
sharan767 <127106387+sharan767@users.noreply.github.com>
2026-04-15 10:34:26 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from groq import Groq
+
+st.set_page_config("ACE_AI Content Generator",layout="wide")
+st.title("ACE_AI - Content Generator")
+st.image("ACE_AI_Transparent.jpg")
